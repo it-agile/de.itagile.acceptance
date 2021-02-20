897a219f047616e117a8113dbde0b571df18850a
Sebastian <sebastiankeller@Sebastians-MBP.fritz.box>
2021-02-20 22:12:04 +0100

    Added Javadoc

--- src/test/java/de/itagile/acceptance/pageobjects/helper/SeleniumHelper.java
+++ src/test/java/de/itagile/acceptance/pageobjects/helper/SeleniumHelper.java
@@ -5,6 +5,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class SeleniumHelper {
+	/**
+	 * Use if element is not clickable ("Element is not clickable at point...")
+	 * <pre>
+	 * // Call it like this. Use By.id or what every you need to find the element
+	 * SeleniumHelper.jsClick(driver.findElement(By.id("theUniqueIdOfTheElement")), driver);
+	 * </pre>
+	 * @param element the element you want to click on
+	 * @param driver the WebDrive to use as JavascriptExecutor, usually the usual WebDriver you are already using
+	 */
 	public static void jsClickOn(WebElement element, WebDriver driver) {
 		JavascriptExecutor jse = (JavascriptExecutor) driver;
 		jse.executeScript("arguments[0].click();", element);
